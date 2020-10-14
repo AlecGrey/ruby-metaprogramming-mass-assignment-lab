5c8222ddb0343d20fcd5a203effd0849a5711cc5
AlecGrey <alecgrey.coding@gmail.com>
2020-10-14 13:05:27 -0700

    lab complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-  #your code here
+
+  def initialize(attr)
+    attr.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+  end
+
 end
+
+# alec_traits = {
+#   name: "Alec",
+#   age: 27,
+#   birthdate: "11-17-1992",
+#   favorite_sport: "Ultimate"
+# }
+
+# alec = Person.new(alec_traits)
+
+# puts alec.name, alec.age, alec.birthdate, alec.favorite_sport
